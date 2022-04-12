2f12cc903331ed312bf4c14333e2fc1dc202d255
Sandeep Bagchi <sandeep.bagchi@gmail.com>
2022-04-12 23:52:11 +0530

    Initial Commit

--- advanced_17.cpp
+++ advanced_17.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <memory>
 #include <utility>
+#include <vector>
+#include <map>
 #include "rapidjson/document.h"
 #include "rapidjson/prettywriter.h"
 #include "rapidjson/stringbuffer.h"
@@ -9,7 +11,7 @@
 using Allocator = rapidjson::Document::AllocatorType;
 using ValuePtr = rapidjson::Value *;
 
-template <typename C, typename T>
+template<typename C, typename T>
 struct Props {
   constexpr Props(T C::*member, const char *name, bool required)
       : member{member}, name{name}, required{required} {
@@ -21,56 +23,90 @@ struct Props {
   bool required;
 };
 
-template <typename C, typename T>
+template<typename C, typename T>
 constexpr Props<C, T> prop(T C::*member, const char *name,
                            bool required = false) {
   return Props<C, T>{member, name, required};
 }
 
-template <typename T, T... S, typename F>
+template<typename T, T... S, typename F>
 constexpr void for_each(std::integer_sequence<T, S...>, F &&f) {
   (static_cast<void>(f(std::integral_constant<T, S>{})), ...);
 }
 
-template <typename T>
-void Write(ValuePtr value, Allocator &allocator, T *object) {
-  value->SetObject();
-  constexpr auto props = std::tuple_size<decltype(T::properties)>::value;
-  for_each(std::make_index_sequence<props>{}, [&](const auto &i) {
-    constexpr auto property = std::get<i>(T::properties);
-    using Type = typename decltype(property)::Type;
-    auto key = rapidjson::Value(rapidjson::kStringType);
-    key.SetString(property.name, strlen(property.name));
-    auto val = rapidjson::Value();
-    const auto &ptr = object->*(property.member);
-    if (ptr) {
-      Write(&val, allocator, ptr.get());
+template<typename T>
+struct Write {
+  void operator()(ValuePtr value, Allocator &allocator, const T *object) {
+    value->SetObject();
+    constexpr auto props = std::tuple_size<decltype(T::properties)>::value;
+    for_each(std::make_index_sequence<props>{}, [&](auto i) {
+      constexpr auto property = std::get<i>(T::properties);
+      using Type = typename decltype(property)::Type;
+      auto key = rapidjson::Value(rapidjson::kStringType);
+      key.SetString(property.name, strlen(property.name));
+      auto val = rapidjson::Value();
+      const auto &ptr = object->*(property.member);
+      using P = typename std::remove_reference<decltype(*ptr)>::type;
+      if (ptr) {
+        Write<P>{}(&val, allocator, ptr.get());
+      }
+      value->AddMember(key, val, allocator);
+    });
+  }
+};
+
+template<>
+struct Write<std::string> {
+  void operator()(ValuePtr value, Allocator &allocator, const std::string *object) {
+    value->SetString(object->data(), object->length(), allocator);
+  }
+};
+
+template<>
+struct Write<std::int64_t> {
+  void operator()(ValuePtr value, Allocator &allocator, const std::int64_t *object) {
+    value->SetInt64(*object);
+  }
+};
+
+template<>
+struct Write<double> {
+  void operator()(ValuePtr value, Allocator &allocator, const double *object) {
+    value->SetDouble(*object);
+  }
+};
+
+template<>
+struct Write<bool> {
+  void operator()(ValuePtr value, Allocator &allocator, const bool *object) {
+    value->SetBool(*object);
+  }
+};
+
+template<typename T>
+struct Write<std::vector<T>> {
+  void operator()(ValuePtr value, Allocator &allocator, const std::vector<T> *object) {
+    value->SetArray();
+    for (const auto &item : *object) {
+      auto temp = rapidjson::Value();
+      Write<T>{}(&temp, allocator, &item);
+      value->PushBack(temp, allocator);
     }
-    value->AddMember(key, val, allocator);
-  });
-}
+  }
+};
 
-template <>
-void Write<std::string>(ValuePtr value, Allocator &allocator,
-                        std::string *object) {
-  value->SetString(object->data(), object->length(), allocator);
-}
-
-template <>
-void Write<std::int64_t>(ValuePtr value, Allocator &allocator,
-                         std::int64_t *object) {
-  value->SetInt64(*object);
-}
-
-template <>
-void Write<double>(ValuePtr value, Allocator &allocator, double *object) {
-  value->SetDouble(*object);
-}
-
-template <>
-void Write<bool>(ValuePtr value, Allocator &allocator, bool *object) {
-  value->SetBool(*object);
-}
+template<typename T>
+struct Write<std::map<std::string, T>> {
+  void operator()(ValuePtr value, Allocator &allocator, const std::map<std::string, T> *object) {
+    value->SetObject();
+    for (const auto &item : *object) {
+      auto key = rapidjson::Value(item.first.data(), item.first.length(), allocator);
+      auto val = rapidjson::Value();
+      Write<T>{}(&val, allocator, &item.second);
+      value->AddMember(key, val, allocator);
+    }
+  }
+};
 
 using Allocator = rapidjson::Document::AllocatorType;
 using ValuePtr = rapidjson::Value *;
@@ -96,6 +132,8 @@ class ABC {
   std::unique_ptr<bool> bool_member_;
   std::unique_ptr<double> double_member_;
   std::unique_ptr<DEF> def_member_;
+  std::unique_ptr<std::vector<std::string>> list_member_;
+  std::unique_ptr<std::map<std::string, std::string>> dict_member_;
 
  public:
   constexpr const static auto properties =
@@ -103,7 +141,9 @@ class ABC {
                       prop(&ABC::string_member_, "string_value"),
                       prop(&ABC::double_member_, "double_value"),
                       prop(&ABC::bool_member_, "boolean_value"),
-                      prop(&ABC::def_member_, "def_value"));
+                      prop(&ABC::def_member_, "def_value"),
+                      prop(&ABC::list_member_, "list_value"),
+                      prop(&ABC::dict_member_, "dict_value"));
 };
 
 std::string serialize(const rapidjson::Document &document,
@@ -128,8 +168,13 @@ int main() {
   abc->def_member_->double_member_ = std::make_unique<double>(123.45);
   abc->def_member_->long_member_ = std::make_unique<std::int64_t>(12345);
   abc->def_member_->bool_member_ = std::make_unique<bool>(true);
+  abc->list_member_ = std::make_unique<std::vector<std::string>>();
+  abc->dict_member_ = std::make_unique<std::map<std::string, std::string>>();
+  abc->list_member_->push_back("Hello");
+  abc->list_member_->push_back("World");
+  abc->dict_member_->insert({"Hello", "World"});
 
   rapidjson::Document document;
-  Write(&document, document.GetAllocator(), abc.get());
+  Write<ABC>{}(&document, document.GetAllocator(), abc.get());
   std::cout << serialize(document, true) << std::endl;
 }
